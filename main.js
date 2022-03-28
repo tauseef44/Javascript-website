8a16f5ca09fb476afdb0e85d2b9992f820368818
tauseef44 <82203616+tauseef44@users.noreply.github.com>
2022-03-28 19:24:57 +0500

    website project

---
+++ main.js
@@ -0,0 +1,29 @@
+let cart =document.querySelector("nav .cart")
+let cartSideBar =document.querySelector(".cart-sidebar")
+let closeCart =document.querySelector(".close-cart")
+const burger =document.querySelector(".burger")
+const menuSidebar =document.querySelector(".menu-sidebar")
+const closeMenu =document.querySelector(".close-menu")
+const cartItemsTotal =document.querySelector("noi")
+const cartPriceTotal =document.querySelector(".total-amount")
+const cartUi =document.querySelector(".cart-sidebar .cart")
+const totalDiv =document.querySelector(".total-sum")
+const clearBtn =document.querySelector(".clear-cart-btn")
+const cartContent =document.querySelector(".cart-content")
+
+let Cart=[];
+let buttonsDOM=[];
+cart.addEventListener("click",function(){
+    cartSideBar.style.tranform="translate(0%)"
+    const bodyOverlay=document.createElement("div")
+    bodyOverlay.classList.add("overlay");
+    setTimeout(function(){
+        document.querySelector("body").append(bodyOverlay)
+    },300)
+})
+closeCart.addEventListener("click",function(){
+    cartSideBar.style.tranform="translate(100%)"
+    const bodyOverlay=document.querySelector(".overlay")
+    document.querySelector("body").removeChild(bodyOverlay)
+
+})
